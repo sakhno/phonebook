43cd3a1b84cd4f52e2e6b1652c33678e34d514ca
Anton <sakhno83@gmail.com>
2016-04-21 02:21:26 +0300

    configuration fixes

--- src/main/java/com/lardi/phonebook/config/JacksonDaoConfig.java
+++ src/main/java/com/lardi/phonebook/config/JacksonDaoConfig.java
@@ -6,6 +6,7 @@ import org.apache.log4j.LogManager;
 import org.apache.log4j.Logger;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 import java.io.File;
 import java.io.IOException;
@@ -15,12 +16,24 @@ import java.util.HashMap;
  * @author Anton Sakhno <antonsakhno.work@gmail.com>
  */
 @Configuration
+@Profile("default")
 public class JacksonDaoConfig {
     private static final Logger LOGGER = LogManager.getLogger(JacksonDaoConfig.class);
 
     @Bean
     public File jsonFile() throws IOException {
-        File file = new File("src/main/resources/phonebook.json");
+        //reading path from config file
+        String path = System.getProperty("jsonpath");
+        File file;
+        //if no path in config, creating file in resources
+        if(path==null){
+            file = new File("src/main/resources/phonebook.json");
+        }else {
+            if(!path.endsWith("/")){
+                path = path+"/";
+            }
+            file = new File(path+"phonebook.json");
+        }
         if (file.createNewFile()) {
             JsonPhonebookModel data = new JsonPhonebookModel();
             data.setUsers(new HashMap<>());
